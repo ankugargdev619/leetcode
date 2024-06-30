fb84520f8f3a7a424ba3021f575ecba6471d2013
ankugargdev619 <137602411+ankugargdev619@users.noreply.github.com>
2024-06-30 17:37:49 +0530

    Solution 2 added

--- code/MajorityElement.java
+++ code/MajorityElement.java
@@ -1,4 +1,25 @@
-class Solution {
+/**
+169. Majority Element
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2 
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+**/
+
+class Solution1 {
     public int majorityElement(int[] nums) {
         int majority = nums[0];
         Map<Integer,Integer> count = new HashMap<>();
@@ -13,5 +34,13 @@ class Solution {
             }
         }
         return majority;
+    }
+}
+
+// Solution 2 : This is based on the intuition that the sorted array will have the majority element occupy the n/2 position
+class Solution2 {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
